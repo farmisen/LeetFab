641587bb8ca9da0cf38eefae3bcb2384786044fa
Fabrice <farmisen@gmail.com>
2024-12-13 10:15:02 -0800

    Update file 12/13/2024, 10:15:02 AM

--- 259-3SumSmaller/259-3SumSmaller.ts
+++ 259-3SumSmaller/259-3SumSmaller.ts
@@ -1,27 +1,55 @@
 // Complexity: O(n^3)
 
+// const threeSumSmaller = (nums: number[], target: number): number => {
+//   nums.sort((a, b) => a - b);
+//   let result = 0;
+
+//   for (let i = 0; i < nums.length - 2; i++) {
+//     // stop early when finding triplets w/ sum < target is impossible
+//     if (target > 0 && nums[i] >= target) {
+//       continue;
+//     }
+//     let startIdx = i + 1;
+//     let endIdx = nums.length - 1;
+
+//     while (startIdx < nums.length - 1) {
+//       const sum = nums[i] + nums[startIdx] + nums[endIdx];
+//       if (sum < target) {
+//         result++;
+//       }
+//       endIdx--;
+//       if (startIdx === endIdx) {
+//         startIdx++;
+//         endIdx = nums.length - 1;
+//       }
+//     }
+//   }
+
+//   return result;
+// };
+
+// can be simplified when realizing that the count of possible triplets 
+// between startIdx and endIdx with idx fixed is endIdx - startIdx 
+
 const threeSumSmaller = (nums: number[], target: number): number => {
   nums.sort((a, b) => a - b);
   let result = 0;
 
   for (let i = 0; i < nums.length - 2; i++) {
+    // stop early when finding triplets w/ sum < target is impossible
     if (target > 0 && nums[i] >= target) {
       continue;
     }
     let startIdx = i + 1;
     let endIdx = nums.length - 1;
 
-    while (startIdx < nums.length - 1) {
+    while (startIdx < endIdx) {
       const sum = nums[i] + nums[startIdx] + nums[endIdx];
-    //   console.log(`(${i}, ${startIdx}, ${endIdx})`, sum)
-
       if (sum < target) {
-        result++;
-      }
-      endIdx--;
-      if (startIdx === endIdx) {
-        startIdx++;
-        endIdx = nums.length - 1;
+        result += endIdx - startIdx
+        startIdx++
+      } else {
+        endIdx--;
       }
     }
   }
