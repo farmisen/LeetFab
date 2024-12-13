f6e73421e9db769e52d246bc2e38bb85e135feac
Fabrice <farmisen@gmail.com>
2024-12-13 13:53:26 -0800

    Update file 12/13/2024, 1:53:26 PM

--- 2593-FindScoreofanArrayAfterMarkingAllElements/2593-FindScoreofanArrayAfterMarkingAllElements.ts
+++ 2593-FindScoreofanArrayAfterMarkingAllElements/2593-FindScoreofanArrayAfterMarkingAllElements.ts
@@ -1,24 +1,20 @@
-// complexity O(nlogn) for sorting and O(n) for main loop hence O(nlogn_
+// complexity O(nlogn) for sorting and O(n) for main loop hence O(nlogn)
 
 const findScore = (nums: number[]): number => {
     const sortedNumsMap = nums.map((value, index) => [value, index])
-    sortedNumsMap.sort((a, b) => a[0] - b[0])
-
+    // sort by inc value then by inc index
+    sortedNumsMap.sort((a, b) => a[0] - b[0] || a[1] - b[1]);
+    // use idx Set to track marked distinct numbers
+    const marked = new Set<number>()
     let score = 0
-    for (const [_, idx] of sortedNumsMap) {
-        const val = nums[idx]
-        if (val < 0) {
+    for (const [val, idx] of sortedNumsMap) {
+        if (marked.has(idx)) {
             continue
         }
         score += val
-        nums[idx] = -1
-        if (nums[idx - 1]) {
-            nums[idx - 1] = -1
-        }
-        if (nums[idx + 1]) {
-            nums[idx + 1] = -1
-        }
-
+        marked.add(idx)
+        marked.add(idx+1)
+        marked.add(idx-1)
     }
 
     return score
