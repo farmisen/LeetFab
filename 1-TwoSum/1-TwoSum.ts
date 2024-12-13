eb13217ac0883275c466c9987889301133f05830
Fabrice <farmisen@gmail.com>
2024-12-12 21:42:29 -0800

    Update file 12/12/2024, 9:42:29 PM

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -2,29 +2,14 @@
 
 const twoSum = (nums: number[], target: number): number[] => {
 
-    const numsMap = new Map<number, number[]>()
-    nums.forEach((n, idx) => {
-        if (numsMap.has(n)) {
-            numsMap.set(n, [...numsMap.get(n), idx])
-        } else {
-            numsMap.set(n, [idx])
-        }
-    })
-
-
+    const numsMap = new Map<number, number>()
+    
     for (let idx = 0; idx < nums.length; idx++) {
         const n = nums[idx]
         if (numsMap.has(target - n)) {
-            const targetIdxs = numsMap.get(target - n)
-
-            const [idx1, idx2] = n === target - n && targetIdxs.length > 2
-                ? targetIdxs
-                : [idx, targetIdxs[0]]
-
-            if (idx1 !== idx2) {
-                return [idx1, idx2]
-            }
+            return [idx, numsMap.get(target - n)]
         }
+        numsMap.set(n, idx)
     }
 
     return []
