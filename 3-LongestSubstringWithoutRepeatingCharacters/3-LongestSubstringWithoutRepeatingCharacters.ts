590014b5c3ffa6122508b6e4d4c7f4c3d66deb7e
Fabrice <farmisen@gmail.com>
2024-12-12 19:05:22 -0800

    Create file

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.ts
@@ -0,0 +1,19 @@
+const lengthOfLongestSubstring = (s: string): number => {
+    let startIdx = 0
+    let longuestSubstringLength = 0
+    while( startIdx + longuestSubstringLength < s.length) {
+        const substring = new Set()
+        let endIdx = startIdx
+        do {
+            const c = s.charAt(endIdx)
+            if (substring.has(c)) {
+                break
+            }
+            substring.add(c)
+            endIdx++        
+        } while (endIdx < s.length)
+        startIdx++
+        longuestSubstringLength = Math.max(longuestSubstringLength, substring.size)
+    }
+    return longuestSubstringLength
+};
