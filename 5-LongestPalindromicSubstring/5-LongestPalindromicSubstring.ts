1afad38a2edd6aa58f9776db7697bc6ac134c136
Fabrice <farmisen@gmail.com>
2024-12-18 18:00:46 -0800

    Update file 12/18/2024, 6:00:46 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.ts
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.ts
@@ -1,3 +1,5 @@
+// Complexity O(n^2)
+
 
 const longestPalindrome = (s: string): string => {
 
@@ -12,19 +14,19 @@ const longestPalindrome = (s: string): string => {
   }
 
   // if palCache[i][j] is true then the substring starting at i and ending at j is a palidrome
-  const palCache: boolean[][] = Array.from({ length: strLen }, () => Array.from({ length: strLen }, () => undefined))
+  const palIndex: boolean[][] = Array.from({ length: strLen }, () => Array.from({ length: strLen }, () => false))
 
-  let maxIdx = 0
+  let startIdx = 0
   let maxLen = 1
 
   for (let i = 0; i < strLen; i++) {
-    palCache[i][i] = true
+    palIndex[i][i] = true
   }
 
   for (let i = 0; i < strLen - 1; i++) {
     if (s[i] === s[i + 1]) {
-      palCache[i][i + 1] = true
-      maxIdx = i
+      palIndex[i][i + 1] = true
+      startIdx = i
       maxLen = 2
     }
   }
@@ -33,14 +35,14 @@ const longestPalindrome = (s: string): string => {
   for (let len = 3; len <= strLen; len++) {
     for (let i = 0; i < strLen - len + 1; i++) {
       const j = i + len - 1
-      if (s[i] === s[j] && palCache[i + 1][j - 1] === true) {
-        palCache[i][j] = true
+      if (s[i] === s[j] && palIndex[i + 1][j - 1] === true) {
+        palIndex[i][j] = true
         maxLen = len
-        maxIdx = i
+        startIdx = i
       }
     }
   }
 
 
-  return s.slice(maxIdx, maxLen + maxIdx)
+  return s.slice(startIdx, maxLen + startIdx)
 }
