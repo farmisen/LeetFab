136b9291c4953d2e844b42b55da9214db20970bf
Fabrice <farmisen@gmail.com>
2024-12-19 13:16:57 -0800

    Update file 12/19/2024, 1:16:57 PM

--- 2832-MaximalRangeThatEachElementIsMaximuminIt/2832-MaximalRangeThatEachElementIsMaximuminIt.ts
+++ 2832-MaximalRangeThatEachElementIsMaximuminIt/2832-MaximalRangeThatEachElementIsMaximuminIt.ts
@@ -1,4 +1,4 @@
-const maximumLengthOfRanges = (nums: number[]): number[] => {
+const maximumLengthOfRangesV1 = (nums: number[]): number[] => {
   // for each nums[i]
   // starts at i and grow left and right while nums[i-1] and nums[i+1] < nums[i]
   // while growing toward the right
@@ -65,3 +65,46 @@ const maximumLengthOfRanges = (nums: number[]): number[] => {
 
   return results
 };
+
+
+// now trying to use a monotonic stack (hinted from dicussions)
+
+const maximumLengthOfRanges = (nums: number[]): number[] => {
+  const nextGreaterStack: number[] = []
+  const prevGreaterStack: number[] = []
+  const nextGreaterIdx: number[] = Array.from({ length: nums.length })
+  const prevGreaterIdx: number[] = Array.from({ length: nums.length })
+
+  for (let i = 0; i < nums.length; i++) {
+    // console.log("----------", i)
+    // console.log("1", nextGreaterIdx, nextGreaterStack)
+    // console.log("1", prevGreaterIdx, prevGreaterStack)
+
+    while (nextGreaterStack.length !== 0 && nums[i] > nums[nextGreaterStack[nextGreaterStack.length - 1]]) {
+      nextGreaterIdx[nextGreaterStack.pop()] = i
+    }
+    nextGreaterStack.push(i)
+
+    while (prevGreaterStack.length !== 0 && nums[i] > nums[prevGreaterStack[prevGreaterStack.length - 1]]) {
+      prevGreaterStack.pop()
+    }
+    if (prevGreaterStack.length !== 0 && nums[prevGreaterStack[prevGreaterStack.length - 1]] > nums[i]) {
+      prevGreaterIdx[i] = prevGreaterStack[prevGreaterStack.length - 1]
+    }
+    prevGreaterStack.push(i)
+    // console.log("2", nextGreaterIdx, nextGreaterStack)
+    // console.log("2", prevGreaterIdx, prevGreaterStack)
+  }
+
+  const results = []
+  for (let i = 0; i < nums.length; i++) {
+    const leftIdx = prevGreaterIdx[i] !== undefined ? prevGreaterIdx[i] + 1 : 0
+    const rightIdx = nextGreaterIdx[i] !== undefined ? nextGreaterIdx[i] - 1 : nums.length - 1
+    results.push(rightIdx - leftIdx + 1)
+  }
+  // console.log(results)
+  // console.log(prevGreaterIdx)
+  // console.log(nextGreaterIdx)
+
+  return results
+}
