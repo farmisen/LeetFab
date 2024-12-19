bf21aff3065227f098810cdb200a38e6badce3fb
Fabrice <farmisen@gmail.com>
2024-12-18 20:25:53 -0800

    Create file

---
+++ 20-ValidParentheses/20-ValidParentheses.ts
@@ -0,0 +1,38 @@
+// lets start a the begining of the str
+
+
+
+const complement = {
+  "(": ")",
+  ")": "(",
+  "[": "]",
+  "]": "[",
+  "{": "}",
+  "}": "{",
+}
+
+const opening = new Set<string>(["(", "[", "{"])
+
+
+const isValid = (s: string): boolean => {
+  const stack = Array<string>()
+
+  for (let i=0; i<s.length; i++) {
+    const cur = s[i]
+    const top = stack[stack.length - 1]
+    if (cur === complement[top]) {
+      stack.pop()
+      continue
+    } 
+
+    if (opening.has(cur)) {
+      stack.push(cur)
+    } else {
+      return false
+    }
+
+  }
+
+
+  return stack.length === 0
+};
