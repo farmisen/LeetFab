2602a65b3b68918cfa6d5cdf4126a0c41e152ecd
Fabrice <farmisen@gmail.com>
2024-12-19 13:14:54 -0800

    Update file 12/19/2024, 1:14:54 PM

--- 2832-MaximalRangeThatEachElementIsMaximuminIt/2832-MaximalRangeThatEachElementIsMaximuminIt.ts
+++ 2832-MaximalRangeThatEachElementIsMaximuminIt/2832-MaximalRangeThatEachElementIsMaximuminIt.ts
@@ -25,13 +25,10 @@ const maximumLengthOfRanges = (nums: number[]): number[] => {
     let left = idx
     let right = idx
     const val = nums[idx]
-    // console.log("-----", i)
 
     while (true) {
-      // !stopping conditions
       const canMoveLeft = left > 0 && nums[left - 1] < val
       const canMoveRight = right < nums.length && nums[right + 1] < val
-      // console.log("1", left, right)
       if (canMoveLeft) {
         if (nums[left - 1] < nums[left]) {
           left -= results[left - 1]
@@ -51,9 +48,7 @@ const maximumLengthOfRanges = (nums: number[]): number[] => {
       if (!canMoveLeft && !canMoveRight) {
         break
       }
-      // console.log("2", left, right)
     }
-    // console.log("+", left, right)
     const len = right - left + 1
     results[idx] = len
     return len
