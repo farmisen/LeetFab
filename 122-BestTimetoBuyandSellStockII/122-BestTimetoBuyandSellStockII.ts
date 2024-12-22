47988e30c79c913567a7221cdace1585ef359398
Fabrice <farmisen@gmail.com>
2024-12-21 16:17:25 -0800

    Update file 12/21/2024, 4:17:25 PM

--- 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.ts
+++ 122-BestTimetoBuyandSellStockII/122-BestTimetoBuyandSellStockII.ts
@@ -1,26 +1,3 @@
-// const maxProfit = (prices: number[]): number => {
-//   let minPrice = prices[0]
-//   let maxPrice = prices[0]
-//   let profit = 0
-
-
-//   for (let i = 1; i < prices.length; i++) {
-//       const price = prices[i]
-//       if (price < minPrice) {
-//         minPrice = price
-//       }
-//       if (price < maxPrice) {
-//         maxPrice = price
-//       }
-//       if ( price - minPrice > profit) {
-//         profit = price - minPrice
-//       }
-//   }
-
-//   return profit
-// }
-
-
 const maxProfit = (prices: number[]): number => {
   let profit = 0
 
@@ -40,5 +17,4 @@ const maxProfit = (prices: number[]): number => {
   }
 
   return profit
-
 }
