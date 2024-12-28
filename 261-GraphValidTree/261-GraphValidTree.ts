7ee78a2fc5b98e1d55ee4319c28c2b23e3a194fa
Fabrice <farmisen@gmail.com>
2024-12-27 21:16:55 -0800

    Update file 12/27/2024, 9:16:55 PM

--- 261-GraphValidTree/261-GraphValidTree.ts
+++ 261-GraphValidTree/261-GraphValidTree.ts
@@ -23,11 +23,6 @@ const hasLoop =  (graph: number[][], visited: boolean[]): boolean => {
 
 
 const validTree = (len: number, edges: number[][]): boolean => {
-
-  // remove nodes connected to only another node until you can't
-  // if there is nodes left then not a tree
-  // use an array of array to represent the graph
-
   const graph: number[][] = Array.from({ length: len }, () => [])
 
   edges.forEach((edge) => {
@@ -44,30 +39,5 @@ const validTree = (len: number, edges: number[][]): boolean => {
 
   // check if all the nodes have been visited
   return !visited.some(v => !v)
-
-  // check if all the nodes are part of the same graph
-  // if (isPartitioned(graph)) {
-  //   return false
-  // }
-
-  // // remove leaves while possible
-  // while (true) {
-  //   let leafCount = 0
-  //   for (let n = 0; n < len - 1; n++) {
-  //     if (graph[n].length === 1) {
-  //       const m = graph[n].pop()
-  //       graph[m] = graph[m].filter((o) => o !== n)
-  //       if (graph[m].length === 1) {
-  //         leafCount += 1
-  //       }
-  //     }
-  //   }
-  //   if (leafCount === 0) {
-  //     break
-  //   }
-  // }
-
-  // // it's not a tree if there are still nodes in the graph
-  // console.log(graph)
-  // return !graph.some(n => n.length > 0)
+  
 }
